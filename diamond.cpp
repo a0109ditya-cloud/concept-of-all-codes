3247fb37420a769955b268d474ae5b7ff3dee4ab
a0109ditya-cloud <a0109ditya@gmail.com>
2026-09-26 00:22:59 +0530

    Add diamond shape printing program in C++
    Implement a program to print a diamond shape based on user input.

---
+++ diamond.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    for(int i=1;i<=n;i++){
+        for(int s=1;s<=n-i;s++){
+            cout << " ";
+        }
+        for(int j=1;j<=2*i-1;j++){
+            cout << j;
+        }cout << endl;
+    }
+    for(int i=n-1;i>=1;i--){
+        for(int space =1;space<=n-i;space++){
+            cout << " ";
+        }
+        for(int j=1;j<=2*i-1;j++){
+            cout << "*";
+        }cout << endl;
+        
+    }
+}
